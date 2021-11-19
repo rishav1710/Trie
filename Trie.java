184f3be6f6a4851a687cc4f031c014d06050dfe3
Rishav Kalra <75754555+rishav1710@users.noreply.github.com>
2021-11-19 16:58:14 +0530

    Create Trie.java

---
+++ Trie.java
@@ -0,0 +1,46 @@
+class Trie{
+    final static int ALPH_SIZE=26;
+   
+    class TrieNode{
+        TrieNode[] children=new TrieNode[ALPH_SIZE];
+        boolean isEndOfWord;
+        TrieNode(){
+            
+            for(int i=0;i<ALPH_SIZE;i++)
+            children[i]=null;
+            
+            isEndOfWord=false;
+        }
+    }
+     TrieNode root;
+     Trie(){
+         root=new TrieNode();
+     }
+    void insert(String key){
+        
+        TrieNode temp=root;
+        int index;
+        for(int i=0;i<key.length();i++){
+            index=key.charAt(i)-'a';
+            if(temp.children[index]==null)
+            temp.children[index]=new TrieNode();
+            
+            temp=temp.children[index];
+        }
+        temp.isEndOfWord=true;
+    }
+    
+    boolean search(String key){
+        TrieNode temp=root;
+        int index;
+        for(int i=0;i<key.length();i++){
+            index=key.charAt(i)-'a';
+            if(temp.children[index]==null)
+            return false;
+            
+            temp=temp.children[index];
+        }
+        return temp.isEndOfWord;
+    }
+    
+}
